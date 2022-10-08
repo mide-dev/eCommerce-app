48829781fceecfd515774851a6794619d5b7bfc2
midedev <ibrahimayomide330@gmail.com>
2022-10-08 07:29:23 +0100

    build category component and install scss for styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from "react";
+import CategoryItem from "./components/category-item/CategoryItem";
+import { categories } from "./constants";
+import "./categoriesContainer-styles.scss";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="categories-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} {...category} />
+      ))}
+    </div>
+  );
 };
 
 export default App;
