5b60565c97e3507fa8f36090d12f74501cce8c9c
midedev <ibrahimayomide330@gmail.com>
2022-10-11 21:25:46 +0100

    build signUp component

--- src/utils/firebase/Firebase.jsx
+++ src/utils/firebase/Firebase.jsx
@@ -28,13 +28,13 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, provider);
 
 export const db = getFirestore();
 
 export const createUserDocumentFromAuth = async (userAuth) => {
   const userDocRef = doc(db, "users", userAuth.uid);
-
-  console.log(userDocRef);
 
   const userSnapshot = await getDoc(userDocRef);
 
